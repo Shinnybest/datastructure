72853f70f71fc66e75a820a6e73cce36d2652345
Shinnybest <kim295521@naver.com>
2022-06-01 11:10:57 +0900

    선택정렬

--- sort/selectionsort.py
+++ sort/selectionsort.py
@@ -1,10 +1,15 @@
 # my code
+# 최소값을 항상 제일 왼쪽으로 보내는 것
 def selectionsort(lst):
     for i in range(len(lst)-1):
-        min_num = lst[i]
+        minimum = lst[i]
         for j in range(i+1, len(lst)):
-            if lst[j] < min_num:
-                index_v, min_num = j, lst[j]
-        if lst[i] != min_num:
-            lst[i], lst[index_v] = lst[i], min_num
+            if lst[j] < minimum:
+                idx, minimum = j, lst[j]
+        if lst[i] != minimum:
+            lst[i], lst[idx] = lst[idx], lst[i]
     return lst
+
+
+selectionsort([2, 5, 6, 7, 8, 9, 1, 3, 4, 10])
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
